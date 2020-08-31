f71cc29bb9c342d912e0d72718d27429e558732f
Vinay-Prb <vinayprabhakar91@gmail.com>
2020-08-31 17:15:39 +0530

    Final_Parsing_File

--- xml.c
+++ xml.c
@@ -1,65 +1,49 @@
-#include <stdio.h>
-#include <string.h>
-#include <libxml/parser.h>
-#include <libxml/tree.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<libxml/parser.h>
+#include<libxml/tree.h>
+
 int main()
 {
-    xmlDoc         *document;
-    xmlNode        *root, *first_child, *node,*temp;
-    char           *filename;
-    
-   
-    filename = "bmd.xml";
+xmlDoc *doc;
+xmlNode *root,*first_child,*temp,*node;
+char *filename;
 
-    document = xmlReadFile(filename, NULL, 0);
-    temp = xmlDocGetRootElement(document);
-    root=xmlFirstElementChild(temp);
-  
-        char* arrayName[8];
-        char* arrayValue[8];
-        int i=0;
-        first_child = root->children;
-        for (node = first_child; node; node = node->next) {
-            // if(node->type==1)
-            // fprintf(stdout, "\t Child is <%s>  (%i) value: \n", node->name ,node->type);
-            // // temp=node;
-
-            if(node->type==1)
-            {
-            // printf("%d\n",checkNamespace("MessageID",node->name));
-                arrayName[i]=(char*)node->name;
-                arrayValue[i]=(char*)xmlNodeGetContent(node);
-                //fprintf(stdout, "attributes:<%s>\tvalue: %s\n", node->name,xmlNodeGetContent(node));
-                //printf("%d: name->%s value->%s\n",i,arrayName[i],arrayValue[i]);
-                i++;
-            }
-        }
-
-    root=xmlLastElementChild(temp);
-    first_child = root;
-        for (node = first_child; node; node = node->next) {
-            // if(node->type==1)
-            // fprintf(stdout, "\t Child is <%s>  (%i) value: \n", node->name ,node->type);
-            // // temp=node;
-
-            if(node->type==1)
-            {
-            // printf("%d\n",checkNamespace("MessageID",node->name));
-                arrayName[i]=(char*)node->name;
-                arrayValue[i]=(char*)xmlNodeGetContent(node);
-                //fprintf(stdout, "attributes:<%s>\tvalue: %s\n", node->name,xmlNodeGetContent(node));
-                //printf("%d: name->%s value->%s\n",i,arrayName[i],arrayValue[i]);
-                i++;
-            }
-        }
-    // }
-    int size = sizeof arrayName / sizeof arrayName[0];
-    for(int i=0;i<=size;i++)
-    {
-        printf("%d: name->%s value->%s\n",i,arrayName[i],arrayValue[i]);
-    }
-    
-
-    fprintf(stdout, "...\n");
-    return 0;
-}
+filename="bmd.xml";
+doc=xmlReadFile(filename,NULL,0);
+ temp=xmlDocGetRootElement(doc);
+ root=xmlFirstElementChild(temp);
+ 
+ char* arrayname[8];
+ char *arrayvalue[8];
+ int i=0;
+ first_child=root->children;
+ for(node=first_child;node;node=node->next)
+ {
+ if(node->type==XML_ELEMENT_NODE)
+ {
+ arrayname[i]=(char*)node->name;
+ arrayvalue[i]=(char*)xmlNodeGetContent(node);
+ i++;
+ }
+ }
+ root=xmlLastElementChild(temp);
+ first_child=root;
+ for(node=first_child;node;node=node->next)
+ {
+ if(node->type==XML_ELEMENT_NODE)
+ {
+ 
+ arrayname[i]=(char*)node->name;
+ arrayvalue[i]=(char*)xmlNodeGetContent(node);
+ }
+ }
+ 
+ int size=sizeof arrayname/sizeof arrayname[0];
+ for(int i=0;i<=size;i++)
+ {
+ printf("%d : name->%s value->%s \n",i,arrayname[i],arrayvalue[i]);
+ }
+ fprintf(stdout,"... \n");
+ return 0;
+ }
