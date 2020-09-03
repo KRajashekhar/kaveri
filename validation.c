ac3f5d346715939f3eee51abb2159e54b8da2b29
KRajashekhar <2016eeb1081@iitrpr.ac.in>
2020-09-03 17:09:21 +0530

    bmd_validation

--- validation.c
+++ validation.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include "db_access1.h"
-#include "parsing1.h"
+#include "parsing.h"
+#include "db_access.h"
+#define MAX_STRING 200
 
 
 /*void finish_with_error(MYSQL *conn)
@@ -15,7 +16,8 @@ int is_bmd_valid(bmd b)
 {
  int valid = 1; // 1 => vaild, -1 => invalid
     // TODO: Implement the validation logic here
-      bmd_envelop envl;
+      bmd_envelop envl = b.envelop;
+
      if(envl.message_id==NULL || envl.message_type==NULL || envl.sender_id==NULL || envl.destination_id==NULL || envl.received_on==NULL || envl.signature==NULL || envl.reference_id==NULL || envl.user_properties==NULL)
     {
         valid=-2;
@@ -27,11 +29,17 @@ int is_bmd_valid(bmd b)
         fprintf(stderr,"mysql_init() failed\n");
         exit(1);
     }
-    if(mysql_real_connect(conn,"localhost","root","prabhakars 589b","esb_db",0,NULL,0)==NULL)
+    if(mysql_real_connect(conn,"localhost","raja","Kucharla@1","esb_db",0,NULL,0)==NULL)
     {
         finish_with_error(conn);
     }
-    if(mysql_query(conn,"select route_id from routes where sender==envl.sender_id"))
+     
+     int return_value;
+     char quer[MAX_STRING];
+     return_value = snprintf(quer,MAX_STRING,"select route_id from routes where sender='%s'",envl.sender_id);
+    char query[return_value+1];
+    return_value = snprintf(query,return_value+1,"select route_id from routes where sender='%s'",envl.sender_id);
+    if(mysql_query(conn,query))
     {
         finish_with_error(conn);
     }
@@ -67,5 +75,7 @@ int main()
 {
     bmd b;
     b=parse_bmd_xml("bmd.xml");
-    int ans =is_bmd_valid(b);
+    int ans;
+    ans =is_bmd_valid(b);
+    printf("%d\n",ans);
 }
