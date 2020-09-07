602657c7b2cce6cd8cc2faf2c92dde04c060fe50
Vinay-Prb <vinayprabhakar91@gmail.com>
2020-09-07 13:50:12 +0530

    unit testing

---
+++ esb_app/src/db_access.h
@@ -0,0 +1,91 @@
+#ifndef _queue_the_request
+#define _queue_the_request
+
+//gcc connect.c -o connect 'mysql_config --cflags --libs'
+
+#include <string.h>
+#include <mysql/mysql.h>
+#define MAX_STRING 200
+
+void finish_with_error(MYSQL *con) {
+    fprintf(stderr,"%s\n", mysql_error(con)) ;
+    mysql_close(con);
+    exit(1);
+}
+
+int db_access(bmd b) {
+    
+    MYSQL *con = mysql_init(NULL);
+      
+      if (con == NULL) {
+          fprintf(stderr, "%s \n",mysql_error(con));
+          exit(1);
+      }
+
+      if(mysql_real_connect(con, "localhost" ,
+      "root", "prabhakars 589b" , "esb_db", 0 , NULL , CLIENT_MULTI_STATEMENTS )==NULL) {
+
+          finish_with_error(con);
+      }
+     
+     bmd_envelop e = b.envelop;
+     size_t len = strlen(e.received_on);
+
+     char date[len+2];
+     int i;
+     for(i = 0; i <len-2 ; i++) {
+        
+        if(e.received_on[i] == 'T') {
+
+        date[i] = ' ';
+        }
+        else {
+         date[i] = e.received_on[i];
+        }
+     }
+    // printf("%d,%zu \n",i,len);
+      date[i] = ':';
+      i++;
+      for(;i<(len+2);i++) {
+          date[i]= e.received_on[i-1];
+      }
+      date[i] = '\0';
+    //  printf("%s\n",date);
+      e.received_on = date;
+
+      
+     char quer[MAX_STRING];
+     int  return_value;
+  //const char array[9][50] = {"sender_id", "dest_id", "message_type", "reference_id", "message_id", "received_on", "status", };
+    return_value = snprintf(quer, MAX_STRING, "INSERT INTO esb_request (sender_id, dest_id, message_type, reference_id, message_id, received_on, status) VALUES ('%s','%s','%s','%s','%s','%s','available')", e.sender_id, e.destination_id, e.message_type, e.reference_id, e.message_id, e.received_on);
+       //printf("return_value = %d\n",return_value);
+       char query[return_value+1];
+     return_value = snprintf(query,return_value+1, "INSERT INTO esb_request (sender_id, dest_id, message_type, reference_id, message_id, received_on, status) VALUES ('%s','%s','%s','%s','%s','%s','available')", e.sender_id, e.destination_id, e.message_type, e.reference_id, e.message_id, e.received_on);
+       
+       //printf("query is %s\n",query);
+       
+      return_value = snprintf(quer,MAX_STRING, "INSERT INTO routes(sender, destination, message_type, is_active) VALUES ('%s','%s','%s',B'1')",e.sender_id, e.destination_id, e.message_type);
+      char query1[return_value+1];
+      return_value = snprintf(query1, return_value+1,"INSERT INTO routes(sender, destination, message_type, is_active) VALUES ('%s','%s','%s',B'1')",e.sender_id, e.destination_id, e.message_type);
+       //printf("query is %s\n",query1);
+
+        if (mysql_query(con, query1)) {
+
+         finish_with_error(con);
+      }
+    
+      
+      if (mysql_query(con, query)) {
+           
+           finish_with_error(con);
+      }
+
+    
+      int id = mysql_insert_id(con);
+
+      printf("The last inserted row id is: %d\n",id);
+      mysql_close(con);
+      return 0;
+}
+
+#endif
--- esb_app/src/esb/test_esb.c
+++ esb_app/src/esb/test_esb.c
@@ -1,13 +1,22 @@
 #include "munit.h"
 #include "esb.h"
+#include<string.h>
+#include<dirent.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include "parser.h"
 
 /**
  * If the name of a test function is "test_abc" then you should
  * define the setup and teardown functions by the names:
  * test_abc_setup and test_abc_tear_down respectively.
  */
-static void *
-test1_setup(const MunitParameter params[], void *user_data)
+ 
+ 
+ 
+ 
+ static void *
+test_parse_bmd_xml_setup(const MunitParameter params[], void *user_data)
 {
     /**
      * Return the data that will be used for test1. Here we
@@ -16,11 +25,37 @@ test1_setup(const MunitParameter params[], void *user_data)
      * has to be cleaned up in corresponding tear down function,
      * which in this case is test1_tear_down.
      */
-    return strdup("/path/to/bmd.xml");
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("./Testing");
+    char **s;
+    s = malloc(100 * sizeof(char *));
+    int i = 0;
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            char *file = strdup(dir->d_name);
+            if(file[0]=='.')
+              continue;
+            char cwd[100];
+            getcwd(cwd, sizeof(cwd));
+            char path[100];
+            sprintf(path, "%s/Testing/%s", cwd, file);
+             
+            s[i] = strdup(path);
+
+            free(file);
+            i++;
+        }
+        closedir(d);
+    }
+
+    return s;
 }
 
 static void
-test1_tear_down(void *fixture)
+test_parse_bmd_xml_tear_down(void *fixture)
 {
     /* Receives the pointer to the data if that that was created in
     test1_setup function. */
@@ -28,55 +63,82 @@ test1_tear_down(void *fixture)
 }
 
 static MunitResult
-test1(const MunitParameter params[], void *fixture)
+test_parse_bmd_xml(const MunitParameter params[],void *fixture)
 {
-    char *str = (char *)fixture;
-    /**
-     * Perform the checking of logic here as needed.
-     * Typically, you will invoke the function under testing
-     * here by passing it suitable data from fixture. Then,
-     * use assertions to verify the expected results.
-     * In this example, we are just checking the value of a
-     * string which we were expecting to be available in the
-     * fixture itself. This test will fail when you change the
-     * string. You will need to recompile and re-run the tests
-     * to see the effect of any changes in data in this example.
-     */
-    munit_assert_string_equal(str, "/path/to/bmd.xml");
-
-    // Invoke the ESB function (or any other function to test)
-    int status = process_esb_request(str);
-    
-    // Assert the expected results
-    munit_assert_true(status == 0);
-    return MUNIT_OK;
+	char **str =(char **)fixture;
+	int i=0;
+	while(str[i]!=NULL)
+	{
+	bmd *bmd;
+	bmd=parse_bmd_xml(str[i]);
+	munit_assert_true(bmd!=NULL);
+	free(bmd);
+	i++;
+	}
+	return MUNIT_OK;
 }
 
-/* Define the setup and the test for test2 */
 static void *
-test2_setup(const MunitParameter params[], void *user_data)
+test_filesize_setup(const MunitParameter params[], void *user_data)
 {
-    return strdup("TEST-2");
+	char file[]"Payload.json";
+	int *p=malloc(sizeof(int *));
+	*p=get_filesize(file);
+	return p;
 }
 
-static void
-test2_tear_down(void *fixture)
+//teardown
+
+static void test_filesize_tear_down(void *fixture)
 {
-    free(fixture);
+	free(fixture);
 }
 
 static MunitResult
-test2(const MunitParameter params[], void *fixture)
-{
-    char *str = (char *)fixture;
-    munit_assert_string_equal(str, "TEST-2");
-    return MUNIT_OK;
-}
+test_filesize(cont MunitParameter params[], void *fixture)
+ {
+   int *filesize=(int *)fixture;
+   munit_assert_int(filesize,<,500000);
+   return MUNIT_OK;
+ }
+ 
+ static void *
+ test2_parse_bmd_xml_setup(const MunitParameter params[], void *user_data)
+ {
+	 char cwd[100];
+	 getcwd(cwd,sizeof(cwd));
+	 char path[100];
+	 sprintf(path,"%s/Testing%s",cwd,"bmd.xml");
+	 return strdup(path);
+ }
+ 
+ static void
+ test2_parse_bmd_xml_tear_down(void * fixture)
+ {
+ 	free(fixture);
+ }
+ 
+ static MunitResult
+ 
+ test2_parse_bmd_xml(const MunitParamter params[], void *fixture)
+ {
+ 	char *str=(char *)fixture;
+ 	bmd *bmd;
+ 	bmd=parse_bmd_xml(str);
+ 	munit_asser_string_equal(bmd->bmd_envelop->MessageId,"A9ECAEF2-107A-4452-9553-043B6D25386E");
+ 	munit_asser_string_equal(bmd->bmd_envelpo->Sender,"756E2EAA-1D5B-4BC0-ACC4-4CEB669408DA");
+ 	munit_assert_string_equal(bmd->bmd_envelop->Destination,"6393F82F-4687-433D-AA23-1966330381FE");
+ 	munit_assert_string_equal(bmd->bmd_envelop->MessageType,"CreditReport");
+ 	munit_assert_string_equal(bmd->bmd->envelop->Signature,"63f5f61f7a79301f715433f8f3689390d1f5da4f855169023300491c00b8113c");
+ 	munit_assert_string_equal(bmd->bmd_envelop->RefernceID,"INV-PROFILE-889712");
+ 	munit_assert_string_equal(bmd->bmd_envelop->CreationDateTime,"2020-08-12T05:18:00+0000");
+ 	return MUNIT_OK;
+ }
 
 /* Put all unit tests here. */
 MunitTest esb_tests[] = {
     {
-        "/my-test-1",   /* name */
+        "/test_parse_bmd_xml",   /* name */
         test1,  /* test function */
         test1_setup,    /* setup function for the test */
         test1_tear_down,    /* tear_down */
@@ -84,12 +146,20 @@ MunitTest esb_tests[] = {
         NULL                    /* parameters */
     },
     {
-        "/my-test-2",   /* name */
+        "/test_bmd_parse_xml",   /* name */
         test2,  /* test function */
         test2_setup,    /* setup function for the test */
         test2_tear_down,    /* tear_down */
         MUNIT_TEST_OPTION_NONE, /* options */
         NULL                    /* parameters */
+    },
+    {
+    "/test_json_filesize",
+    test_filesize,
+    test_filesize_setup,
+    test_filesize_tear_down,
+    MUNIT_TEST_OPTION_NONE,
+    NULL
     },
     /* Mark the end of the array with an entry where the test
    * function is NULL */
