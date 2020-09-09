fe2ed4adfe3a9a7885fe2e63e13f5d0e9e61f9e4
Vinay-Prb <vinayprabhakar91@gmail.com>
2020-09-09 19:07:37 +0530

    http

--- esb_app/src/http/http_get.c
+++ esb_app/src/http/http_get.c
@@ -4,10 +4,16 @@ int main()
 {
 	CURL *curl;
 	CURLcode res;
+    struct curl_slist *headers=NULL; 
+   headers = curl_slist_append(headers, "Accept: application/json");  
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    headers = curl_slist_append(headers, "charset: utf-8"); 
 	curl_global_init(CURL_GLOBAL_ALL);
 	curl=curl_easy_init();
 	if(curl)
 	{
+
+curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 		curl_easy_setopt(curl,CURLOPT_URL,"https://ifsc.razorpay.com/");
 		curl_easy_setopt(curl,CURLOPT_HTTPGET,"IDIB000V086");
 		res=curl_easy_perform(curl);
