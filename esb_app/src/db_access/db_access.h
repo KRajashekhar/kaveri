d948eb251480c5e25dc37449cb576dd53f6d81c1
KRajashekhar <2016eeb1081@iitrpr.ac.in>
2020-09-15 23:08:55 +0530

    db_access

--- esb_app/src/db_access/db_access.h
+++ esb_app/src/db_access/db_access.h
@@ -18,6 +18,7 @@
 #define VALID 1
 #define INVALID -1
 
+
 int insert_into_esb_request(char *sender_id, char *dest_id,
 char *message_type, char *reference_id, char *message_id,
 char *received_on, char *data_location, char *status, char *status_details);
