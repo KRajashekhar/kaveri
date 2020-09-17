d4caa738fbd1b5b30771d0270d5840d13c74201d
Vinay-Prb <vinayprabhakar91@gmail.com>
2020-09-17 14:27:36 +0530

    checking in

---
+++ esb_app/src/bmd/xmljson.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+#include"bmd.h"
+
+void xml2json(char Pay[],char Sender[])
+	{
+		char *filename;
+		strcpy(filename,Sender);
+		strcat(filename,"Paylaod.json");
+		FILE *file;
+	    	file = fopen(filename,"w");
+	    	
+	    	if(file == NULL) 
+	    	{
+			printf("file opening failed");
+			exit(0);
+	    	}
+	    	
+	    	
+	    	fprintf(file,"{\n \"Payload\":\"%s\"\n}",Pay);
+	    	
+	    	printf("\nPayload json File created\n");
+	   	
+	    	fclose(file);
+	    	
+	    	//return strdup(filename);
+	}
+int get_filesize(char filename[])
+{
+	FILE *fp;
+	int size=0;
+	fp=fopen(filename,"r");
+	
+	if(fp==NULL)
+	{
+		printf("File opening failed\n");
+	}
+	else
+	{
+		printf("file opened\n");
+	}
+	//file pointer to the end of file
+	
+	fseek(fp,0,2);
+	size=ftell(fp);
+	
+	if(size>5000000)
+	{
+		printf("File size larger than 5 mb not allowed\n");
+	}
+	fclose(fp);
+	
+	return size;
+}
+
+char *get_filecontents (char filename[])
+{
+	long f_size;
+	char *str_data;
+	size_t file_size,result;
+	FILE *fp=fopen(filename,"r");
+	fseek(fp,0,2);
+	
+	//read size of file after moving pointer to the end of teh file
+	f_size=ftell(fp);
+	fseek(fp,0,0);
+	file_size=sizeof(char) * f_size;
+	str_data=malloc(file_size);
+	
+	//returns number of items actually read
+	result =fread(str_data,1,f_size,fp);
+	return strdup(str_data);
+}
+	
+char* xmltojson(char Pay[])
+{
+	char filename[]="Payload.json";
+	FILE *file;
+	file=fopen(filename,"w");
+	
+	if(file==NULL)
+	{
+		printf("File opening failed\n");
+		exit(0);
+	}
+	
+	//write into json file
+	fprintf(file,"{\n \"Paylaod\":\"%s\"\n}",Pay);
+	
+	printf("Payload json file created\n");
+	
+	//close the file
+	fclose(file);
+	
+	return strdup(filename);
+	}
+	
--- esb_app/src/esb/test_esb.c
+++ esb_app/src/esb/test_esb.c
@@ -1,5 +1,9 @@
 #include "munit.h"
 #include "esb.h"
+#include<string.h>
+#include<dirent.h>
+#include<stdio.h>
+#include "xml2json.c"
 
 /**
  * If the name of a test function is "test_abc" then you should
@@ -7,7 +11,7 @@
  * test_abc_setup and test_abc_tear_down respectively.
  */
 static void *
-test1_setup(const MunitParameter params[], void *user_data)
+test_parse_bmd_xml(const MunitParameter params[], void *user_data)
 {
     /**
      * Return the data that will be used for test1. Here we
@@ -16,21 +20,45 @@ test1_setup(const MunitParameter params[], void *user_data)
      * has to be cleaned up in corresponding tear down function,
      * which in this case is test1_tear_down.
      */
-    return strdup("/path/to/bmd.xml");
-}
-
-static void
-test1_tear_down(void *fixture)
-{
-    /* Receives the pointer to the data if that that was created in
-    test1_setup function. */
-    free(fixture);
-}
-
+     DIR *d;
+     struct dirent *dir;
+     d=opendir("./Testing");
+     char **s;
+     s-malloc(100*sizeof(char *));
+     int i=0;
+     if(d)
+     {
+     	while((dir=readdir(d))!=NULL)
+     	{
+     		char *file=strdup(dir->d_name);
+     		if(file[0]=='.')
+     		{
+     			continue;
+     		}
+     		char cwd[100];
+     		getcwd(cwd,sizeof(cwd));
+     		char path[100];
+     		sprintf(path,"%s/Testing/%s",cwd,file);
+     		s[i]=strdup(path);
+     		free(file);
+     		i++;
+     	}
+     	closedir(d);
+     }
+     return s;
+    }
+    static void test_parse_bmd_xml_tear_down(void *fixture)
+    {
+    
+    	//receives the pointer to data if that data was created in test1_setup function
+    	free(fixture);
+    }
+    
+    
 static MunitResult
 test1(const MunitParameter params[], void *fixture)
 {
-    char *str = (char *)fixture;
+    char **str = (char **)fixture;
     /**
      * Perform the checking of logic here as needed.
      * Typically, you will invoke the function under testing
@@ -42,7 +70,17 @@ test1(const MunitParameter params[], void *fixture)
      * string. You will need to recompile and re-run the tests
      * to see the effect of any changes in data in this example.
      */
-    munit_assert_string_equal(str, "/path/to/bmd.xml");
+     int i=0;
+     while(str[i]!=NULL)
+     {
+     	bmd *bmd;
+     	bmd=parse_bmd_xml(str[i]);
+     	munit_asser_true(bmd!=NULL);
+     	
+     	free(bmd);
+     	i++;
+     }
+    //munit_assert_string_equal(str, "/path/to/bmd.xml");
 
     // Invoke the ESB function (or any other function to test)
     int status = process_esb_request(str);
@@ -54,25 +92,146 @@ test1(const MunitParameter params[], void *fixture)
 
 /* Define the setup and the test for test2 */
 static void *
-test2_setup(const MunitParameter params[], void *user_data)
+test2_parse_bmd_xml(const MunitParameter params[], void *user_data)
 {
-    return strdup("TEST-2");
+    char cwd[100];
+    getcwd(cwd,sizeof(cwd));
+    char path[100];
+    sprintf(path,"%s/Testing/%s",cwd,"bmd.xml");
+    
+    return strdup(path);
+ }
 }
 
 static void
-test2_tear_down(void *fixture)
+test2_parse_bmd_xml_tear_down(void *fixture)
 {
     free(fixture);
 }
 
 static MunitResult
-test2(const MunitParameter params[], void *fixture)
+test2_parse_bmd_xml(const MunitParameter params[], void *fixture)
 {
     char *str = (char *)fixture;
+    bmd *bmd;
+    bmd=parse_bmd_xml(str);
+    munit_assert_string_equal(bmd->bmd_envelope->MessageID,"A9ECAEF2-107A-4452-9553-043B6D25386E");
+    munit_assert_string_equal(bmd->bmd_envelope->Sender,"756E2EAA-1D5B-4BC0-ACC4-4CEB669408DA");
+    munit_assert_string_equal(bmd->bmd_envelope->Destination,"6393F82F-4687-433D-AA23-1966330381FE");
+    munit_assert_string_equal(bmd->bmd_envelope->MessageType,"CreditReport");
+    munit_assert_string_equal(bmd->bmd_envelope->Signature,"63f5f61f7a79301f715433f8f3689390d1f5da4f855169023300491c00b8113c");
+    munit_assert_string_equal(bmd->bmd_envelope->ReferenceID,"INV-PROFILE-889712");
+    munit_assert_string_equal(bmd->bmd_envelope->CreationDateTime,"2020-08-12T05:18:00+0000");
+    return MUNIT_OK;
     munit_assert_string_equal(str, "TEST-2");
     return MUNIT_OK;
 }
 
+//test setup for paylaod.json file
+
+static void * test_filesize_setup(const MunitParameter params[],void *user_data)
+{
+	char file[] ="Paylaod.json";
+	int *p=malloc(sizeof(int *));
+	*p=get_filesize(file);
+	return p;
+}
+
+//teardown
+
+static void test_filesize_tear_down(void *fixture)
+{
+	free(fixture);
+}
+
+static MunitResult
+test_filesize(const MunitParameter params[],void *fixture)
+{
+	int *filesize=(int *)fixture;
+	munit_assert_int(*filesize,<,5000000);
+	return MUNIT_OK;
+}
+
+//test setup function for json content 
+
+static void *Json_filecontents_setup(const MunitParameter params[], void *user_data)
+{
+	char *file_created=xml2json("001-01-1234");
+	char *json_data=get_filecontents(file_created);
+	return strdup(json_data);
+}
+
+
+//teardown
+static void Json_filecontents_tear_down(void *fixture)
+{
+	free(fixture);
+}
+
+static MunitResult test_Json_filecontents (const Munitparameter params[],void *fixture)
+{
+	char *json_data=(char *)fixture;
+	char *test_data=get_filecontents("Paylaod.json");
+	
+	munit_assert_string_equal(json_data,test_data);
+	
+	return MUNIT_OK;
+}
+
+//test function for HTTP transport service
+
+test_HTTP_transport_service(const MunitParameter params[],void *fixture)
+{
+	int status=Apply_transport_service("ttps://ifsc.razorpay.com/HDFC0CAGSBK", "HTTP");
+	munit_assert_int(status,==,1);
+	return MUNIT_OK;
+}
+//test fucntion for email transport servicde
+
+static MunitResult test_email_transport_service(const MunitParameter params[], void *fixture)
+{
+	int status=Apply_transport_service("testmailv1@gmail.com","email");
+	munit_asser_int(status,==,1);
+	return MUNIT_OK;
+}
+
+//test function for no transport service
+
+static MunitResult test_no_transport_service(const MunitParameter params[],void *fixture)
+{
+	int status=Apply_tranport_service("email","service");
+	munit_assert_int(status,==,0);
+	return MUNIT_OK;
+}
+
+//test function for http json transform
+
+static MunitResult(const MunitParameter params[],void *fixture)
+{
+	char type[]="Json_transform";
+	int route_id=10;
+	char *transport_key=;
+	char *transport_value="HTTP";
+	char *SENDER;
+	int transport_status=check_transform(type,route_id,transport_key,transport_value,SENDER);
+	munit_asser_int(transform_status,==,1);
+	return MUNIT_OK;
+}
+
+//test function for email json transform
+
+static MunitResult(const MunitParameter params[], void *fixture)
+{
+	char type[]="Json_transform";
+	int route_id=2;
+	char *transport_key;
+	char *transport_value="email";
+	char *SENDER;
+	int transform_status=Apply_transform_service(type,route_id,transport_key,transport_value,SENDER);
+	munit_asser_int(status,==,1);
+	return MUNIT_OK;
+}
+	
 /* Put all unit tests here. */
 MunitTest esb_tests[] = {
     {
