5d374da98559c69c29b45ef805633e0ca9ee9ceb
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-19 15:42:51 +0530

    Update xmljson.c

--- esb_app/src/bmd/xmljson.c
+++ esb_app/src/bmd/xmljson.c
@@ -1,106 +1,92 @@
-
-// @authors Kucharla Rajashekhar and Vinay Prabhakar
-
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include"bmd.h"
+/* Create a Payload file for a sender */
+void xml2json(char Pay[], char Sender[]) {
+    char * filename;
+    strcpy(filename, Sender);
+    strcat(filename, "Payload.json");
+    FILE * file;
+    file = fopen(filename, "w");
 
-#define SEEK_SET 0
-#define SEEK_CUR 1
-#define SEEK_END 2
+    if (file == NULL) {
+        printf("file opening failed");
+        exit(0);
+    }
 
-void xml2json(char Pay[],char Sender[])
-	{
-		char *filename = " ";
-		strcpy(filename,Sender);
-		strcat(filename,"Paylaod.json");
-		FILE *file;
-	    	file = fopen(filename,"w");
-	    	
-	    	if(file == NULL) 
-	    	{
-			printf("file opening failed");
-			exit(0);
-	    	}
-	    	
-	    	
-	    	fprintf(file,"{\n \"Payload\":\"%s\"\n}",Pay);
-	    	
-	    	printf("\nPayload json File created\n");
-	   	
-	    	fclose(file);
-	    	
-	    	//return strdup(filename);
-	}
-int get_filesize(char filename[])
-{
-	FILE *fp;
-	int size=0;
-	fp=fopen(filename,"r");
-	
-	if(fp==NULL)
-	{
-		printf("File opening failed\n");
-	}
-	else
-	{
-		printf("file opened\n");
-	}
-	//file pointer to the end of file
-	
-	fseek(fp,0,2);
-	size=ftell(fp);
-	
-	if(size>5000000)
-	{
-		printf("File size larger than 5 mb not allowed\n");
-	}
-	fclose(fp);
-	
-	return size;
+    /* Writes into json file */
+    fprintf(file, "{\n \"Payload\":\"%s\"\n}", Pay);
+
+    printf("\nPayload json File created\n");
+
+    /* Closes file */
+    fclose(file);
 }
 
-char *get_filecontents (char filename[])
-{
-	long f_size;
-	char *str_data;
-	size_t file_size;
-	FILE *fp=fopen(filename,"r");
-	fseek(fp,0,2);
-	
-	//read size of file after moving pointer to the end of teh file
-	f_size=ftell(fp);
-	fseek(fp,0,0);
-	file_size=sizeof(char) * f_size;
-	str_data=malloc(file_size);
-	
-	//returns number of items actually read
-	fread(str_data,1,f_size,fp);
-	return strdup(str_data);
+/* Get the filesize for a given filename */
+int get_filesize(char filename[]) {
+    /*File size validation*/
+    FILE * fp;
+    int size = 0;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL)
+        printf("\nFile unable to open ");
+    else
+        printf("\nFile opened ");
+    fseek(fp, 0, 2); /* file pointer at the end of file */
+    size = ftell(fp); /* take a position of file pointer un size variable */
+
+    /*Check if Filesize is larger than 5MB*/
+    if (size > 5000000)
+        printf("\nFile size larger than 5MB not allowed!\n");
+
+    /*close the file*/
+    fclose(fp);
+    /*return filesize*/
+    return size;
 }
-	
-char* xmltojson(char Pay[])
-{
-	char filename[]="Payload.json";
-	FILE *file;
-	file=fopen(filename,"w");
-	
-	if(file==NULL)
-	{
-		printf("File opening failed\n");
-		exit(0);
-	}
-	
-	//write into json file
-	fprintf(file,"{\n \"Paylaod\":\"%s\"\n}",Pay);
-	
-	printf("Payload json file created\n");
-	
-	//close the file
-	fclose(file);
-	
-	return strdup(filename);
+
+/* Get the filecontents for a given filename */
+char * get_filecontents(char filename[]) {
+    long f_size;
+    char * str_data;
+    size_t file_size, result;
+    FILE * fp = fopen(filename, "r");
+    fseek(fp, 0, SEEK_END);
+
+    /* Reads the size of the file */
+    f_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    file_size = sizeof(char) * f_size;
+    str_data = malloc(file_size);
+
+    /* fread returns number of items actually read. */
+    result = fread(str_data, 1, f_size, fp);
+    return strdup(str_data);
+
 }
-	
+
+/* This function is defined for testing */
+char * xmltojson(char Pay[]) {
+    char filename[] = "Payload.json";
+    FILE * file;
+    file = fopen(filename, "w");
+
+    if (file == NULL) {
+        printf("file opening filed");
+        exit(0);
+    }
+
+    /* Writes into json file */
+    fprintf(file, "{\n \"Payload\":\"%s\"\n}", Pay);
+
+    printf("\nPayload json File created\n");
+
+    /* Closes file */
+    fclose(file);
+
+    /* returns json filename */
+    return strdup(filename);
+}
