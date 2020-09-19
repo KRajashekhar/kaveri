8ebd0263ec053fa54775787bdcc469d334e873a5
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-19 15:41:30 +0530

    Update bmd.h

--- esb_app/src/bmd/bmd.h
+++ esb_app/src/bmd/bmd.h
@@ -1,46 +1,42 @@
+#ifndef bmd_parser
+#define bmd_parser
+#define START_TAG 1 //value acoording to return type of xmlTextReaderNodeType
+#define END_TAG 15
 
-// @authors Kucharla Rajashekhar and Vinay Prabhakar
+typedef struct {
+    const unsigned char * key;
+    void * value;
+}
+pair;
 
-#include<stdio.h>
-#ifndef BMD_H
-#define BMD_H
+typedef struct {
+    const unsigned char * MessageID;
+    const unsigned char * MessageType;
+    const unsigned char * Sender;
+    const unsigned char * Destination;
+    const unsigned char * CreationDateTime;
+    const unsigned char * Signature;
+    pair * UserProperties[50];
+    const unsigned char * ReferenceID;
 
-struct _bmd_envelop {
-    char* sender_id;
-    char* destination_id;
-    char* message_type;
-    // TODO: Other fields
-    char* reference_id;
-    char* message_id;
-    char * received_on;
-    char * signature;
-    char * user_properties;
-};
+}
+envelope;
 
-typedef struct _bmd_envelop bmd_envelop;
+typedef struct {
+    const unsigned char * data;
+}
+payload;
 
-struct _bmd{
-    bmd_envelop envelop;
-    char* payload;
-};
+typedef struct {
+    envelope * bmd_envelope;
+    payload * bmd_payload;
+}
+BMD;
 
-typedef struct _bmd bmd;
+BMD * parse_bmd_xml(char * );
+BMD * process_xml(char * );
+int check_if_string_is_guid(const unsigned char * value);
+envelope * extract_envelop(char * bmd_xml);
+payload * extract_payload(char * bmd_xml);
 
-bmd parse_bmd_xml(char * bmd_file_path) ;
-int is_bmd_valid(bmd b);
-void xml2json(char Pay[],char Sender[]);
-int get_filesize(char filename[]);
-char *get_filecontents (char filename[]);
-char* xmltojson(char Pay[]);
-int send_mail(char *to,char *file_path);
-int check_transform(char type[], int route_id, char* transport_key, char* transport_value, char* SENDER);
-void HTTP_Json_transform(int route_id, char* transport_key, char *SENDER);
-static int Apply_transport_service(char URL[], char transport_service[]);
-
-//bmd *parse_bmd_xml(char *);
-//bmd *process_xml(char *);
-//int check_if_string_is_guid(const unsigned char *value);
-//bmd_envelop *extract_envelop(char *bmd_xml);
-//payload *extract_payload(char *bmd_xml);
-
-#endif
+#endif /* bmd_parser.h */
