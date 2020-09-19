2851b92bbae626ae9a7aa6ab999b2f23c6744d39
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-19 15:08:34 +0530

    Update esb.h

--- esb_app/src/esb/esb.h
+++ esb_app/src/esb/esb.h
@@ -1,27 +1,5 @@
-
-// @authors Kucharla Rajashekhar and Vinay Prabhakar
-
-#include<stdio.h>
-///#include "../bmd/bmd.h"
-#ifndef ESB_H
-#define ESB_H
-
-// task tag
-
-typedef struct task
-	{
-		int id;
-		char *sender;
-		char *destination;
-		char *message_type;
-		char *data_location;
-		int processing_attempts;
-	} task_t;
-	
 int process_esb_request(char* bmd_file_path);
 //int insert_in_esb_request(BMD *);
-//int select_active_route(const unsigned char *Sender, const unsigned char *Destination, const unsigned char *MessageType);
-//int select_transport_config(int route_id);
-//int select_transform_config(int route_id);
-
-#endif
+int select_active_route(const unsigned char *Sender, const unsigned char *Destination, const unsigned char *MessageType);
+int select_transport_config(int route_id);
+int select_transform_config(int route_id);
