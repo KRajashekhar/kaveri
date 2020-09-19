957d6798c75c3e8e03d9e1926e2108cc5c4d74f3
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-19 17:12:40 +0530

    Added path to bmdparser

--- esb_app/src/test/test_bmd_parser.c
+++ esb_app/src/test/test_bmd_parser.c
@@ -4,7 +4,7 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "bmd_parser.h"
+#include "../bmd/bmd_parser.h"
 
 
 /*gcc test_esb.c munit.c bmd_parser.c database.c esb.c  `mysql_config --cflags --libs` `xml2-config --cflags --libs` -o test_esb
