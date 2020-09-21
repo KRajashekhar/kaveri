76aa05849f16a5b2c0543c8d6382922476624096
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-21 14:24:31 +0530

    querying the status for MessageID

--- esb_app/src/esb_rest.c
+++ esb_app/src/esb_rest.c
@@ -4,20 +4,24 @@
 
 typedef struct
 {
-	int status;
+	int success;
+	char* status;
 	char *message_id;
 }rest_result;
 
 int esb_rest(struct http_request *);
 rest_result get_messageID(struct http_request *);
+
 //Receives a request
 int esb_rest(struct http_request *req)
 {
     printf("Received the REST request.\n");
     rest_result get_messageid =get_messageID(req);
     
-    if(get_messageid.status>0)
+    if(get_messageid.success>0)
     {
+       printf("Message ID: %s\n",get_messageid.message_id);
+       printf("Status: %s\n",get_messageid.status);
        return KORE_RESULT_OK;
     }
     else
@@ -31,11 +35,13 @@ int esb_rest(struct http_request *req)
 rest_result get_messageID(struct http_request *req)
 {
     rest_result rs_res;
-    rs_res.status=1; //1-->OK , -ve for errors
+    rs_res.success=1; //1-->OK , -ve for errors
+    rs_res.status="available"; 
     http_populate_get(req);
     if(req->method!=HTTP_METHOD_GET)
     {
-        rs_res.status=-1;
+        rs_res.success=-1;
+        rs_res.status="error";
         kore_log(LOG_ERR, "Rejecting non GET request.");
 		http_response(req, 405,"Wrong Method\n", 13);
         return rs_res;
@@ -46,11 +52,15 @@ rest_result get_messageID(struct http_request *req)
     if(http_argument_get_string(req,"MessageID",&out))
     {
        rs_res.message_id=malloc(strlen(out)*sizeof(char)+1);
+       rs_res.status=malloc(strlen(out)*sizeof(char)+1);
        strcpy(rs_res.message_id,out);
+       char status_details[20];
+       get_status(out, status_details);
+       strcpy(rs_res.status,(char *)status_details);
     }
        else
        {
-          rs_res.status=-1;
+          rs_res.success=-1;
        }
        
     return rs_res;  
