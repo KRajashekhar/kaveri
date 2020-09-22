b1984e440d4015ef4b0cc342c0dbbad0f7d04c36
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-22 15:56:01 +0530

    Added SFTP service

--- esb_app/src/esb/transport.c
+++ esb_app/src/esb/transport.c
@@ -10,10 +10,11 @@ int Apply_transport_service(char URL[], char transport_service[]) {
         send_http_request(URL);
         return status;
     } else if (!strcmp(transport_service, "email")) {
+        printf("\nemail service applied\n");
         char filename[100];
         printf("Enter file to read:\t");
-        scanf("%s", & filename);
-        printf("\nemail service applied\n");
+        scanf("%s", &filename);
+        
         int mail_status = send_mail(URL, filename);
         if (mail_status == 0) {
             printf("Mail sent\n");
@@ -22,6 +23,14 @@ int Apply_transport_service(char URL[], char transport_service[]) {
             printf("Failed to send the Mail\n");
             status = 0;
         }
+    } else if (!strcmp(transport_service, "SFTP")) {
+        printf("\nSFTP service applied\n");
+        char filename[100];
+        printf("Enter file to read:\t");
+        scanf("%s", &filename);
+        
+        sftp_upload(URL, filename);
+        return status;
     } else {
         status = 0;
         printf("\nNo service found\n");
