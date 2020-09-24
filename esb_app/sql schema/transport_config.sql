2e8e8b9ddcc15dce139c8a4e64c8a7537b8fe935
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-24 12:16:54 +0530

    Add files via upload

---
+++ esb_app/sql schema/transport_config.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM esb_db.transport_config
+LIMIT 0, 1000
+
+-- Date: 2020-09-24 12:01
+*/
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2001,1,'https://ifsc.razorpay.com/','HTTP');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2002,2,'testkaveri123@gmail.com','email');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2003,3,'https://ifsc.razorpay.com/','HTTP');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2004,4,'https://ifsc.razorpay.com/','HTTP');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2005,15,'https://ifsc.razorpay.com/','HTTP');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2006,16,'testkaveri123@gmail.com','email');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2007,17,'https://ifsc.razorpay.com/','HTTP');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2008,18,'https://ifsc.razorpay.com/','HTTP');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (2009,19,'Payload.json','SFTP');
