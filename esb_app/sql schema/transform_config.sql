6130d6c092c5ec4c0dcaa75ef3897037cb2d2a3a
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-24 11:59:15 +0530

    SQL commands for transform table

---
+++ esb_app/sql schema/transform_config.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM esb_db.transform_config
+LIMIT 0, 1000
+
+-- Date: 2020-09-24 11:57
+*/
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1001,1,'Json_transform','json');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1002,2,'Json_transform','json');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1003,3,'Json_transform','json');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1004,4,'Json_transform','json');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1005,15,'Json_transform','json');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1006,16,'Json_transform','json');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1007,17,'Json_transform','json');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1008,18,'Json_transform','json');
+INSERT INTO `` (`id`,`route_id`,`config_key`,`config_value`) VALUES (1009,19,'Json_transform','json');
