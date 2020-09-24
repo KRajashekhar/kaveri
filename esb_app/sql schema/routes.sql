744201d1dd51b2847bc6de3f90fde7efcd50d247
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-24 12:17:20 +0530

    Add files via upload

---
+++ esb_app/sql schema/routes.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM esb_db.routes
+LIMIT 0, 1000
+
+-- Date: 2020-09-24 12:00
+*/
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (1,'756E2EAA-1D5B-4BC0-ACC4-4CEB669408DA','6393F82F-4687-433D-AA23-1966330381FE','CreditReport','1');
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (2,'556E2EAA-1D5B-5BC0-BCC4-4CEB669408DA','6323D82F-4687-433D-AA23-1966330381FE','DebitReport','1');
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (3,'666E2EAA-1D5B-5BC0-BCC4-4CEB669408DA','8323D82F-4687-433D-AA23-1966330381FE','DebitReport','1');
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (4,'222E2EAA-1D5B-5BC0-BCC4-4CEB669408DA','2223D82F-4687-433D-AA23-1966330381FE','CreditReport','1');
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (15,'222E2EAA-1D5B-5BC0-BCC4-4CEB669408DA','8323D82F-4687-433D-AA23-1966330381FE','CreditReport','1');
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (16,'522E2EAA-2D5B-8BC0-FCC4-5CEB669408DA','5223D82F-4657-333D-BA23-2966330381FE','CreditReport','1');
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (17,'4ac26e80-f658-11ea-adc1-0242ac120002','4ac271fa-f658-11ea-adc1-0242ac120002','CreditReport','1');
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (18,'5AC37984-B658-11EA-ADC1-0242AC120002','BAC27A42-F658-11EA-adc1-0202AC120002','DebitReport','1');
+INSERT INTO `` (`route_id`,`sender`,`destination`,`message_type`,`is_active`) VALUES (19,'44428848-F658-11EA-ABC1-0242ABC20002','4AC22910-D658-11DA-ADC1-0242AC120002','CreditReport','1');
